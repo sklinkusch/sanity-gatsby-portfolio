945fe2d2b54d55b49c68ddc53ed578576c22ecd1
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-10-22 10:55:51 +0200

    add hardcoded hotline section

--- web/src/components/address-section.js
+++ web/src/components/address-section.js
@@ -1,1 +1,15 @@
-import React from 'react';
+import React from "react";
+
+const Addresssection = () => (
+  <div className="container">
+    <div className="hotline">
+      <h3>Hotline</h3>
+      <p>
+        Ruf uns an: <span>+49 511 87458409</span>
+      </p>
+      <p>Mo. - Fr. von 9:00 - 17:00 Uhr</p>
+    </div>
+  </div>
+);
+
+export default Addresssection;
