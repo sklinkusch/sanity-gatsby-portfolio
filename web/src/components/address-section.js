3e2da33fc112e03b919ae36ed60fd61d92ea6dc8
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-10-22 11:02:23 +0200

    add livechat

--- web/src/components/address-section.js
+++ web/src/components/address-section.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React from 'react';
 
 const Addresssection = () => (
-  <div className="container">
-    <div className="hotline">
-      <h3>Hotline</h3>
+  <div className='container'>
+    <div className='hotline addresselement'>
+      <h3 className='addresstitle'>Hotline</h3>
       <p>
         Ruf uns an: <span>+49 511 87458409</span>
       </p>
       <p>Mo. - Fr. von 9:00 - 17:00 Uhr</p>
     </div>
+    <div className='livechat addresselement'>
+      <h3 className='addresstitle'>Live Chat</h3>
+      <p>Benutze das Charfenster unten rechts</p>
+      <p>Mo. - Fr. von 9:00 - 17:00 Uhr</p>
+    </div>
   </div>
-);
+)
 
-export default Addresssection;
+export default Addresssection
