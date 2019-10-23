e3da49b7361392f306c51f926316d2d97f2f72e2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-10-23 11:34:12 +0200

    use variables instead of text

--- web/src/components/address-section.js
+++ web/src/components/address-section.js
@@ -1,29 +1,39 @@
-import React from "react";
-import "./address-section.css";
-import styles from "./layout.module.css";
+import React from 'react'
+import './address-section.css'
 
-const Addresssection = () => (
-  <div className="wrapper">
-    <div className="container">
-      <div className="hotline addresselement">
-        <h3 className="addresstitle">Hotline</h3>
-        <p>
-          Ruf uns an: <span>+49 511 87458409</span>
-        </p>
-        <p>Mo. - Fr. von 9:00 - 17:00 Uhr</p>
-      </div>
-      <div className="livechat addresselement">
-        <h3 className="addresstitle">Live Chat</h3>
-        <p>Benutze das Charfenster unten rechts</p>
-        <p>Mo. - Fr. von 9:00 - 17:00 Uhr</p>
-      </div>
-      <div className="nachricht addresselement">
-        <h3 className="addresstitle">Nachricht</h3>
-        <p>fragen@sleepink.de</p>
-        <p>Wir werden so schnell wie möglich auf deine Nachricht antworten</p>
+const Addresssection = () => {
+  const hotline = 'Hotline'
+  const callUs = 'Ruf uns an: '
+  const phoneNumber = '+49 511 87458409'
+  const openingTimes = 'Mo. - Fr. von 9:00 - 17:00 Uhr'
+  const livechat = 'Live Chat'
+  const useWindow = 'Benutze das Charfenster unten rechts'
+  const message = 'Nachricht'
+  const email = 'fragen@sleepink.de'
+  const replyAsap = 'Wir werden so schnell wie möglich auf deine Nachricht antworten'
+  return (
+    <div className='wrapper'>
+      <div className='container'>
+        <div className='hotline addresselement'>
+          <h3 className='addresstitle'>{hotline}</h3>
+          <p>
+            {callUs}<span>{phoneNumber}</span>
+          </p>
+          <p>{openingTimes}</p>
+        </div>
+        <div className='livechat addresselement'>
+          <h3 className='addresstitle'>{livechat}</h3>
+          <p>{useWindow}</p>
+          <p>{openingTimes}</p>
+        </div>
+        <div className='nachricht addresselement'>
+          <h3 className='addresstitle'>{message}</h3>
+          <p>{email}</p>
+          <p>{replyAsap}</p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+}
 
-export default Addresssection;
+export default Addresssection
