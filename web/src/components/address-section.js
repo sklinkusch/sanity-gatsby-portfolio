666b50c34c65eece1ca5396e3ca29b3c48ecf636
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-10-23 11:21:52 +0200

    apply grid

--- web/src/components/address-section.js
+++ web/src/components/address-section.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import './address-section.css';
-import styles from './layout.module.css';
+import React from "react";
+import "./address-section.css";
+import styles from "./layout.module.css";
 
 const Addresssection = () => (
-  <div className='container'>
-    <div className='hotline addresselement'>
-      <h3 className='addresstitle'>Hotline</h3>
-      <p>
-        Ruf uns an: <span>+49 511 87458409</span>
-      </p>
-      <p>Mo. - Fr. von 9:00 - 17:00 Uhr</p>
-    </div>
-    <div className='livechat addresselement'>
-      <h3 className='addresstitle'>Live Chat</h3>
-      <p>Benutze das Charfenster unten rechts</p>
-      <p>Mo. - Fr. von 9:00 - 17:00 Uhr</p>
-    </div>
-    <div className='nachricht addresselement'>
-      <h3 className='addresstitle'>Nachricht</h3>
-      <p>fragen@sleepink.de</p>
-      <p>Wir werden so schnell wie möglich auf deine Nachricht antworten</p>
+  <div className="wrapper">
+    <div className="container">
+      <div className="hotline addresselement">
+        <h3 className="addresstitle">Hotline</h3>
+        <p>
+          Ruf uns an: <span>+49 511 87458409</span>
+        </p>
+        <p>Mo. - Fr. von 9:00 - 17:00 Uhr</p>
+      </div>
+      <div className="livechat addresselement">
+        <h3 className="addresstitle">Live Chat</h3>
+        <p>Benutze das Charfenster unten rechts</p>
+        <p>Mo. - Fr. von 9:00 - 17:00 Uhr</p>
+      </div>
+      <div className="nachricht addresselement">
+        <h3 className="addresstitle">Nachricht</h3>
+        <p>fragen@sleepink.de</p>
+        <p>Wir werden so schnell wie möglich auf deine Nachricht antworten</p>
+      </div>
     </div>
   </div>
-)
+);
 
-export default Addresssection
+export default Addresssection;
