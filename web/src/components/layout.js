b4f54288106ba9eeaaaf4a05244b21fc1fa34746
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-10-24 14:42:07 +0200

    drill props from Layout to Addresssection

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -5,11 +5,11 @@ import Addresssection from './address-section';
 import '../styles/layout.css';
 import styles from './layout.module.css';
 
-const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
+const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle, phone, email }) => (
   <>
     <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
     <div className={styles.content}>{children}</div>
-    <Addresssection />
+    <Addresssection phone={phone} email={email} />
     <footer className={styles.footer}>
       <div className={styles.footerWrapper}>
         <div className={styles.siteInfo}>
